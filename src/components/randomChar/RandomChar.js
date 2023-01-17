9ab8d9073a2b7d32675b36fa7e14849b190402f8
Danil <danil.miroshnichenko2022@gmail.com>
2023-01-17 19:47:23 +0200

    CharInfo, CharList change

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,10 +9,7 @@ import MarvelService from '../../services/MarvelServices';
 import ErrorMessage from '../errorMessage/Error.Message';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
+
     state = {
         char: {},
         loading: true,
@@ -20,12 +17,25 @@ class RandomChar extends Component {
     }
     marvelService = new MarvelService();
 
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 10000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
+    onCharLoading = () => {
+        this.setState({loading: true})
+    }
+
     onCharLoaded = (char) => {
         this.setState({ char, loading: false })
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
@@ -56,7 +66,8 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main"
+                        onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -68,9 +79,13 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
     const {name, description, thumbnail, homepage, wiki} = char
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -6,20 +6,17 @@ class MarvelService {
 
     getResourse = async (url) => {
         let res = await fetch(url);
-
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status ${res.status}`)
         }
-
         return await res.json();
-
     }
 
     getAllCharacters = async() => {
         const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
-
     }
+    
     getCharacter = async (id) => {
         const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0])
@@ -28,11 +25,13 @@ class MarvelService {
     _transformCharacter = (char) => {
         const descr = char.description ? `${char.description.slice(0,210)}...` : 'No description for this Character'
         return {
+            id: char.id,
             name: char.name,
             description: descr,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
+            comics: char.comics.items.slice(0,10)
         }
     }
 }
