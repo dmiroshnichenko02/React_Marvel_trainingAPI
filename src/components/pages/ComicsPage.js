85b428bdb6a151e35fa08332193dcebc9268f548
Danil <danil.miroshnichenko2022@gmail.com>
2023-02-08 14:40:43 +0200

    React Helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,3 +1,6 @@
+import { Helmet } from "react-helmet";
+
+
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner"
 
@@ -6,6 +9,13 @@ const ComicsPage = () => {
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                />
+                <title>Comics page</title>
+            </Helmet>
             <AppBanner />
             <ComicsList />
         </>
