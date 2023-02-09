e33101c102a94ed77db4cd2e0b4335a1ac5f2d1e
Danil <danil.miroshnichenko2022@gmail.com>
2023-02-09 17:11:57 +0200

    Fix bugs

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import PropTypes from 'prop-types'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
@@ -108,12 +108,17 @@ const CharList = (props) => {
     // const errorMessage = error ? <ErrorMessage /> : null;
     // const spinner = loading && !newItemLoading ? <Spinner /> : null;
 
+
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItem(data), newItemLoading)
+    }, [process]);
+
     return (
         <div className='char__list'>
             {/* {errorMessage}
             {spinner}
             {items} */}
-            {setContent(process, ()=> renderItem(data), newItemLoading)}
+            {elements}
             <button className="button button__main button__long"
                 disabled={newItemLoading}
                 style={{ 'display': charEnded ? 'none' : 'block' }}
--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -24,7 +24,7 @@ const CharSearchForm = () => {
             .then(() => setProcess('confirmed'));
     }
 
-    const errorMessage = process === error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
+    const errorMessage = process === 'error' ? <div className="char__search-critical-error"> <ErrorMessage /> </div> : null;
     const results = !char ? null : char.length > 0 ?
         <div className="char__search-wrapper">
             <div className="char__search-success">There is! Visit {char[0].name} page?</div>
@@ -59,8 +59,7 @@ const CharSearchForm = () => {
                             placeholder="Enter name" />
                         <button
                             type='submit'
-                            className="button button__main"
-                            disabled={loading}>
+                            className="button button__main">
                             <div className="inner">find</div>
                         </button>
                     </div>
